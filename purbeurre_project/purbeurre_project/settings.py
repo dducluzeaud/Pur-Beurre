6aa1d88e40f12eaa783eca0cd985f0b01e241b58
david ducluzeaud <david@davids-MacBook-Pro.local>
2018-04-02 15:12:57 +0200

    Revert "heroku bug"
    This reverts commit a17568bbd11bc597975d98bbffc2fb550748e345.

--- purbeurre_project/purbeurre_project/settings.py
+++ purbeurre_project/purbeurre_project/settings.py
@@ -139,8 +139,8 @@ LOGIN_URL = '/openfoodfacts/login'
 
 if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.normpath(os.path.join(PROJECT_ROOT, 'staticfiles'))
-    STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
